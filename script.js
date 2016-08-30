0c8140694a6273a3185d9fb7796ecee86452d7d6
josephferrairo <josephferrairo@gmail.com>
2016-08-29 21:38:08 -0400

    output now shows employee titles and total earnings

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $.getJSON('https://data.cityofboston.gov/resource/ntv7-hwjm.json', function(data) {
   var output = '<ul>';
   $.each(data, function(key, val) {
-      output += '<li>' + val.name + '</li>';
+      output += '<li>' + val.title + ', ' + val.total_earnings + '</li>';
   });
   output +='</ul>'
   $('#update').html(output);
