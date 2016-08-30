31732624268e773ac5b001df6ab6f73cb7cc5bb1
josephferrairo <josephferrairo@gmail.com>
2016-08-29 22:01:38 -0400

    can search through job titles

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-$.getJSON('https://data.cityofboston.gov/resource/ntv7-hwjm.json', function(data) {
-  var output = '<ul>';
-  $.each(data, function(key, val) {
-      output += '<li>' + val.title + ', ' + val.total_earnings + '</li>';
+$('#search').keyup(function() {
+  var searchField = $('#search').val();
+  //Search using RegExp
+  var myExp = new RegExp(searchField, "i");
+  // Get JSON data
+    $.getJSON('https://data.cityofboston.gov/resource/ntv7-hwjm.json', function(data) {
+        var output = '<ul class ="searchresults">';
+        $.each(data, function(key, val) {
+              //If the search finds a match, output that match
+              if (val.title.search(myExp) != -1) {
+              output += '<li>';
+              output += '<h2>' + val.title + '</h2>';
+              output += '<p>' + '$' + val.total_earnings + '</p>'
+              output += '</li>'
+            }
+        });
+        output += '</ul>';
+        $('#update').html(output);
   });
-  output +='</ul>'
-  $('#update').html(output);
-})
+});
