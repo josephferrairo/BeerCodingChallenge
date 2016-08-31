6be74fb026fec40cbd3de1e66187c28e067fa4f4
josephferrairo <josephferrairo@gmail.com>
2016-08-30 20:24:23 -0400

    pulled earnings length, sum, and average out of the each loop so that the program would run much faster, removed delay on search since it is no longer required

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-var delay = (function(){
-  var timer = 0;
-  return function(callback, ms){
-    clearTimeout (timer);
-    timer = setTimeout(callback, ms);
-  };
-})();
-
 $('#search').keyup(function() {
-  delay(function(){
-    alert('Results Loaded');
-  }, 0 );
   var searchField = $('#search').val();
   //Search using RegExp
   var myExp = new RegExp(searchField, "i");
@@ -17,39 +6,34 @@ $('#search').keyup(function() {
   var beer = "https://data.cityofboston.gov/resource/ntv7-hwjm.json"
   // Get JSON data
   $.getJSON(beer, function(data) {
-
     var output = '<ul class="searchresults">';
-    var earnings = []
-    total_earnings_avg_arr = []
-    $.each(data, function(key, val) {
+    var earnings = [];
+    var total_earnings_avg=0;
 
+    $.each(data, function(key, val) {
       //console.log(earnings)
       //If the search finds a match, output that match
-
       if (val.title.search(myExp) != -1) {
-
         //put total_earnings into an array called earnings
         earnings.push(val.total_earnings);
-        //change earnings from a string to a number
-        earnings = earnings.map(Number);
-        //make a variable for the total amount of earnings
-        var earnings_length = earnings.length;
-        //bulid the sum of the earnings
-        var earnings_sum = earnings.reduce(function(a,b) { return a + b;},0);
-        var total_earnings_avg = (earnings_sum / earnings_length);
-        //modify earnings to only have 2 decimal places
-        var total_earnings_avg = total_earnings_avg.toFixed(2)
-        console.log(total_earnings_avg)
-
-
       }
-
     });
-
-    output += '</ul>';
+    //change earnings from a string to a number
+    earnings = earnings.map(Number);
+    //make a variable for the total amount of earnings
+    var earnings_length = earnings.length;
+    //bulid the sum of the earnings
+    var earnings_sum = earnings.reduce(function(a,b) { return a + b;},0);
+    //divide sum of earnings by the amount of people 'titles'
+    total_earnings_avg = (earnings_sum / earnings_length);
+    //modify earnings to only have 2 decimal places
+    total_earnings_avg = total_earnings_avg.toFixed(2);
+    console.log(total_earnings_avg);
+    output += '<h2>' + '$' + total_earnings_avg + '</h2>'
+    output += '<h3>' + searchField + '</h3>'
     $('#update').html(output);
 
-
+    output += '</ul>';
   });
 
 });
